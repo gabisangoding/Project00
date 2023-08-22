418b5772a4af7c2740651d753add3d9136fd196d
gabisangoding <92967845+gabisangoding@users.noreply.github.com>
2023-08-22 08:45:52 +0700

    Add files via upload

---
+++ WEB PROGRAMMING/HTML/Project 1.0.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>READY TO AGAINTS VOLDEMORT?</title>
+    <center><h1 style="color :rgb(8, 8, 160)" >Hogwarts School of Witchcraft and Wizardry for New Student</h1></center>
+    <hr>
+    <style>
+    .table {
+        background-color: lightblue;
+        font-style: italic;
+        color: darkblue;
+        position: static;
+    }
+
+    
+</style>
+</head>
+<body class="table" >
+    <br>
+        <center>
+        <table  border="0" colspacing="10" colpadding="50" method="post">
+        <div>
+            <form  action="/action_page.php" target="_blank" autocomplete="on" required>
+                    <tr>
+                        <td><label for="fname lname" autofocus><b>Name:</b></label></td>
+                        <td><input type="text" id="name" name="fname" placeholder="First Name" ></td>
+                        <td><input type="text" id="lname" name="lname" placeholder="Last Name"></td>
+                    </tr>
+
+                    <tr>
+                        <td><label for="birthdate"><b>Birthdate:</b></label></td>
+                        <td><input type="date" id="birthdate"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="Address"><b>Address:</b></label></td>
+                        <td><input type="text" id="Address" placeholder="Street"></td> <td><input type="text" id="Address" placeholder="Ward"></td>
+                        
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="text" id="Address" placeholder="Sub-districts"></td>
+                        <td><input type="text" id="Address" placeholder="Districts"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="text" id="Address" placeholder="Province"></td>
+                        <td><input type="text" id="Address" placeholder="Country"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="number" id="Address" placeholder="Postal Code"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="human"><b>Human Blood Type:</b></label></td>
+                        <td><input type="radio" id="Human" name="human">A</td>
+                        <td><input type="radio" id="human" name="human">B</td>
+                        <td><input type="radio" id="Human" name="human">AB</td>
+                        <td><input type="radio" id="Human" name="human">O</td>
+                    </tr>
+                    <tr>
+                        <td><label for="Wizard"><b>Wizard Blood Type:</b></label></td>
+                        <td><input type="radio" id="Wizard" name="wizard" >Muggle-borns</td>
+                        <td><input type="radio" id="Wizard" name="wizard" >Half-bloods</td>
+                        <td><input type="radio" id="Wizard" name="wizard">Pure-bloods</td>
+                    </tr>
+
+                    <tr>
+                        <td><label for="email"><b>Email :</b></label></td>
+                        <td ><input type="email" id="email" name="email" placeholder="Email" ></td>        
+                    </tr>
+                    <tr>
+                        <td><label for="phone"><b>Phone Number:</b></label></td>
+                        <td><input type="number" id="phone" placeholder="Country Code"></td>
+                        <td><input type="phone" id="phone" placeholder="Phone Number" </td>        
+                    </tr>
+                    <tr>
+                        <td><b>Gender</b></td>
+                        <td><input type="radio" id="pria" name="Gender"><label for="pria">Male</label></td>
+                        <td><input type="radio" id="wanita" name="Gender"><label for="wanita">Female</label></td>
+                        <td><input type="radio" id="Non-Binary" name="Gender"><label for="Non-Binary">Non-Binary</label></td>
+                    </tr>
+                    <tr>
+                        <td><b>School Address:</td>
+                        <td colspan="2"><textarea cols="28" rows="6" style="resize: none;" ></textarea></textarea></td></td>
+                    </tr>
+                    <tr>
+                        <td><label for="image"><b>Upload Photo:</b></label></td>
+                        <td><input type="file" id="image" name="image" width="0" height="23" ></td>
+                        <td><input type="reset"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="submit" value="KIRIM"></td>
+                    </tr>
+            
+            </form>
+        </div></center>
+
+
+</body>
+
+
+
+
+</html>
---
+++ WEB PROGRAMMING/PEHAPE/practice.php
@@ -0,0 +1,8 @@
+<?php
+define("greeting", "Welcome to W3Schools.com!", true);
+echo greeting;
+
+define("jIkA", "dimana", false);
+echo "JIkA";
+?>
+?>
